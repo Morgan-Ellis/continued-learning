4b84842ebd6070ef5f654223b2698d5cf9b46e43
Morgan Ellis <morgan.ellis@Ariixs-MacBook-Pro-4.local>
2018-07-09 18:03:15 -0600

    inital commit

---
+++ mean-message-app/backend/app.js
@@ -0,0 +1,42 @@
+const path = require ("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://morgan:YilQHn2arMzHRxmk@cluster0-kwzcx.mongodb.net/test?retryWrites=true"
+  )
+  .then(() => {
+    console.log("Connected to database. ʕง•ᴥ•ʔง");
+  })
+  .catch(() => {
+    console.log("Connection failed. ʕ•́ᴥ•̀ʔ");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use("/images", express.static(path.join("backend/images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+
+module.exports = app;
